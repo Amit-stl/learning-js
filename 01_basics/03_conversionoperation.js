803d81d6b0ada3c5177e29b92828ecc0b8f604e1
Amit-stl <shivink578@gmail.com>
2024-08-12 15:31:33 +0000

    conversion-operation done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "Amit"
+
+//console.log(typeof score);
+
+let valueInnumber = Number(score)
+//console.log(valueInnumber)
+ 
+
+/* jab hum kisi number ko convert karte hai to
+ wo easily convert ho jata hai 33 me
+ "33" => 33 .
+ "33abc" => Nan ye convert nahi hote .
+ true => 1 ; false => 0
+    */
+
+ let isLoggedIn = "Amit"
+ 
+ let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanisLoggedIn);
+
+ // 1 => true ; 0 => false
+ // "" => false
+ // "Amit" => true
+
+ let someNumber = 45
+
+ let stringNumber = String(someNumber)
+ console.log( typeof stringNumber);
+ 
