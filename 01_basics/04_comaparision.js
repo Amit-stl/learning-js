6bd7ec601ff8750bace5d9e43a6daac05a7b417c
Amit-stl <shivink578@gmail.com>
2024-08-13 15:01:11 +0000

    comparison complete

---
+++ 01_basics/04_comaparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//yaha tak to thik hai lekin aage
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+ /* console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+is tarah ke code ko avoid kare and do not create any confusion 
+readiability is priority 
+*/
