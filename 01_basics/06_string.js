cf68302e2c4067eb044db05c75cbe9ee0acebe3e
Amit-stl <shivink578@gmail.com>
2024-08-15 12:30:00 +0000

    string done

---
+++ 01_basics/06_string.js
@@ -0,0 +1,12 @@
+const name = "Amit"  
+//ye ek tarika hua string declare karne ka
+const roll = 21
+
+//console.log( name + roll + " value")
+
+//ths is not modern way of writing code instead
+
+console.log(` hello my name is ${name}and my roll no is ${roll} `)
+
+const gameName = new String ('Velorant')    // (new syntax)  javascript object 
+console.log(gameName)
