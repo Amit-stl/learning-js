1f9e713624c3b1ae284532e3f2015225af4f6985
Amit-stl <shivink578@gmail.com>
2024-08-15 14:33:16 +0000

    string more add on

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -10,3 +10,26 @@ console.log(` hello my name is ${name}and my roll no is ${roll} `)
 
 const gameName = new String ('Velorant')    // (new syntax)  javascript object 
 console.log(gameName)
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
