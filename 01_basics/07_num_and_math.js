fec4a045e0633ba69147cd83d2f7abe7dc4c30f0
Amit-stl <shivink578@gmail.com>
2024-08-16 12:43:47 +0000

    Number and Maths finish

---
+++ 01_basics/07_num_and_math.js
@@ -0,0 +1,12 @@
+const score = 700
+console.log(score)
+
+const balance = new Number(700)
+console.log(balance)
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
