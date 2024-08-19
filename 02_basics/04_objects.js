9d4043c8e830a89e72936e56038848f32512cab2
Amit-stl <shivink578@gmail.com>
2024-08-19 16:06:22 +0000

    objects concept done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+//const tinderuser = new Object()           =>         this is singleton
+const tinderuser = {}                    // =>         this is non-singleton
+
+tinderuser.Id = "123qwer"
+tinderuser.name = "Todoroki"
+tinderuser.isLoggedIn = false
+
+console.log(tinderuser);
+
